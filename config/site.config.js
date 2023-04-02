b9713cce7cfe0afc1a4c071e7bf579a894347bd2
F_TD5X <mjikop1231@gmail.com>
2023-04-02 21:08:20 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -24,7 +24,7 @@ module.exports = {
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
-  maxItems: process.env.MAX_ITEMS ||100,
+  maxItems: process.env.MAX_ITEMS || 100,
 
   // [OPTIONAL] We use Google Fonts natively for font customisations.
   // You can check and generate the required links and names at https://fonts.google.com.
